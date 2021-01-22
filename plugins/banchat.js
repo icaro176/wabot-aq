b83bd69da687974477f196d9f08edd78f5872256
icaro176 <76911826+icaro176@users.noreply.github.com>
2021-01-21 22:28:56 -0300

    banchat.js

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, participants }) => {
   if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.DATABASE._data.chats[m.chat].isBanned = true
-    m.reply('Done!')
-  } else m.reply('Ada nomor host disini...')
+    m.reply('Feito!')
+  } else m.reply('Há um número de host aqui...')
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
