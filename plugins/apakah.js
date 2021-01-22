1ba0e355ab3796d14336cbefd818a12775d89ab8
icaro176 <76911826+icaro176@users.noreply.github.com>
2021-01-21 22:23:45 -0300

    apakah.js

--- plugins/apakah.js
+++ plugins/apakah.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text }) => {
   conn.reply(m.chat, `
-*Pertanyaan:* ${m.text}
-*Jawaban:* ${pickRandom(['Ya','Mungkin iya','Mungkin','Mungkin tidak','Tidak','Tidak mungkin'])}
+*Questão:* ${m.text}
+*Responda:* ${pickRandom(['sim','Talvez sim','Talvez','Provavelmente não','Não','Impossível'])}
 `.trim(), m)
 }
 handler.help = ['apakah <teks>?']
