b10acadedd5cda220a03d6de97da1fb9c81ce490
icaro176 <76911826+icaro176@users.noreply.github.com>
2021-01-21 22:43:06 -0300

    donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,9 +1,9 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [081515860089]
-│ • Telkomsel [081334177340]
+│ •
+│ •
 ╰────
-`.trim()) // Tambah sendiri kalo mau
+`.aparar()) // Adicione você mesmo se quiser
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
